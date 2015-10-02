4200154bfcb0a6b7cd8363df3456a76a21a9c55d
Meng-Jiun, Chiou <coldmanck@gmail.com>
2015-10-03 01:45:20 +0800

    Update standard function of guess game

---
+++ GuessNumberAndroid/app/src/main/java/tw/edu/nctu/coldman519/guessnumberandroid/GuessGame.java
@@ -0,0 +1,54 @@
+package tw.edu.nctu.coldman519.guessnumberandroid;
+
+import android.widget.TextView;
+
+import java.util.Random;
+
+/**
+ * Created by DueMBA on 2015/10/3.
+ */
+public class GuessGame {
+    private int[] answer;
+    private int a, b, digit;
+
+    public void setNumber(int d){
+
+        digit = d;
+        answer = new int[digit];
+
+        Random rand = new Random();
+        for(int i = 0; i < digit; i++)
+            answer[i] = rand.nextInt(10);
+    }
+
+    public int[] startGame(int d, String guess){
+
+
+        int[] guessNumber = getUserInput(guess);
+        int[] returnValue = new int[2];
+
+        for(int i = 0; i < digit; i++){
+            if(answer[i] == guessNumber[i])
+                a++;
+            for(int j = 0; j < digit; j++){
+                if(answer[i] == guessNumber[j] && answer[i] != guessNumber[i])
+                    b++;
+            }
+        }
+
+        returnValue[0] = a;
+        returnValue[1] = b;
+        a = 0;
+        b = 0;
+        return returnValue;
+
+    }
+
+    public int[] getUserInput(String guess){
+        int[] guess_temp = new int[digit];
+        for(int i = 0; i < digit; i++)
+            guess_temp[i] = Integer.valueOf(guess.substring(i, i+1));
+
+        return guess_temp;
+    }
+}
---
+++ GuessNumberAndroid/app/src/main/java/tw/edu/nctu/coldman519/guessnumberandroid/MainActivity.java
@@ -0,0 +1,82 @@
+package tw.edu.nctu.coldman519.guessnumberandroid;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button button;
+    GuessGame guessGame = new GuessGame();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        guessGame.setNumber(4);
+        addListenerOnButton();
+
+//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
+    }
+
+    public void addListenerOnButton() {
+        button = (Button) findViewById(R.id.submit_button);
+        button.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                EditText inputEditText = (EditText) findViewById(R.id.edit_text);
+                String message = inputEditText.getText().toString();
+
+                int[] aAndB = guessGame.startGame(4, message);
+                TextView resultTextView = (TextView) findViewById(R.id.result);
+                resultTextView.append(aAndB[0] + " A " + aAndB[1] + " B\n");
+            }
+        });
+    }
+
+
+
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
